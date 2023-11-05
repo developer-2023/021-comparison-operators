6f0ea9489fd8a82f83f708b3e8f5411711cb5102
developer-2023 <132809345+developer-2023@users.noreply.github.com>
2023-11-05 21:20:50 +0100

    Initial commit

---
+++ 021-comparison-operators.py
@@ -0,0 +1,11 @@
+def main():
+    x = int(input("What's x? "))
+    y = int(input("What's y? "))
+    if x > y:
+        print("x > y")
+    elif x < y:
+        print("x < y")
+    else:
+        print("x = y")
+
+main()
